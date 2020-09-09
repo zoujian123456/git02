6b407a8d673e2623bb328e5e54c489b0740018af
Lenovo <3245201890@qq.com>
2020-09-09 17:46:47 +0800

    hello添加hi

--- git02/src/main/java/cn/yt/git02/Hello.java
+++ git02/src/main/java/cn/yt/git02/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
  public void hello(){
   System.out.println("hello");
   System.out.println("hi");
+  System.out.println("hello word");
  }
  public static void main(String[] args){
   System.out.println("");
