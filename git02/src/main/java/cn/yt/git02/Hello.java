05ffc0d456d7e2e2997e698e78b9938b5c4d62ad
Lenovo <3245201890@qq.com>
2020-09-09 17:10:38 +0800

    创建了一个项目

---
+++ git02/src/main/java/cn/yt/git02/Hello.java
@@ -0,0 +1,10 @@
+package cn.yt.git02;
+
+public class Hello {
+ public void hello(){
+  System.out.println("hello");
+ }
+ public static void main(String[] args){
+  System.out.println("");
+ }
+}
---
+++ git02/src/test/java/cn/yt/git02/Git02ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.yt.git02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Git02ApplicationTests {
+
+ @Test
+ void contextLoads() {
+ }
+
+}
